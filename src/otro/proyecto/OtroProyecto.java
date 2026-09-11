4795b308d69ae558ebe7c56f3541e7114a04e0a7
angieap-code <anngie1313.ap@gmail.com>
2026-09-11 15:56:18 -0600

    Prueba commit

--- src/otro/proyecto/OtroProyecto.java
+++ src/otro/proyecto/OtroProyecto.java
@@ -1,20 +1,30 @@
-/*
+/*Este es el que tenemos correcto con Agie sin Arreglos.
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package otro.proyecto;
+package proyectoreservadehotel;
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
- *
+ * Escenario 1: Registro de huéspedes y reservas
  * @author angie
  */
-public class OtroProyecto {
+public class ProyectoReservadeHotel{
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    //hhh
-}
+        Scanner sc = new Scanner(System.in);
+
+        // Declaración de Variables
+        String nombreHuesped = "";
+        int cantidadNoches = 0;
+        int tipoHabitacion = 0;
+        char confirmacion = '@';
+        double precioHabitacion = 0;
+        double totalReserva = 0;
+        int dia = 0;
+        int mes = 0;
+        int anio = 0;
